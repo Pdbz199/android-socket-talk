3f3f56beb7c2470aa5ee77ae23015deb5024d2cb
Pdbz199 <279pdbz199@gmail.com>
2019-04-27 19:48:36 -0400

    Working chat page + server

---
+++ server.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({
+	extended : false
+}))
+app.use(bodyParser.json())
+var serverPort = app.listen(3000)
+
+app.get('/', function(req, res) {
+    rstream = fs.createReadStream(__dirname + '/chat.html');
+	rstream.pipe(res);
+})
+
+var io = require('socket.io').listen(serverPort)
+
+io.sockets.on('connection', function(socket) {
+    console.log(`${socket.id} connected`)
+
+	socket.on('message', function(data) {
+		socket.emit('message', data)
+    })
+    
+    socket.on('disconnect', function () {
+        console.log(`${socket.id} disconnected`)
+    })
+})
